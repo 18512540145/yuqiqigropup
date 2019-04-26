ee7ce441f7ae33d1c8b24ca843ea950dce7f8797
yuyunbo <1154363414@qq.com>
2019-04-26 21:22:28 +0800

    项目初始化

---
+++ src/main/java/com/yuqiqi/growup/YuqiqiApplication.java
@@ -0,0 +1,13 @@
+package com.yuqiqi.growup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuqiqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuqiqiApplication.class, args);
+	}
+
+}
